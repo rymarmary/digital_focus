9278661121f0f2b229ddc2ecda0f6a8c3decd5a1
Mary <rymarmary@MacBook-Air-Mary.local>
2025-07-29 21:55:19 +0300

    fix types

--- components/AuthGuard.tsx
+++ components/AuthGuard.tsx
@@ -1,24 +1,35 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { supabase } from '@/utils/supabaseClient';
+import { Session } from '@supabase/supabase-js';
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
   const [loading, setLoading] = useState(true);
-  const [session, setSession] = useState<any>(null);
+  const [session, setSession] = useState<Session | null>(null);
   const router = useRouter();
 
   useEffect(() => {
-    const session = supabase.auth.getSession().then(({ data }) => {
-      if (!data.session) router.push('/auth/signin');
-      else setSession(data.session);
-      setLoading(false);
-    });
+    const getSession = async () => {
+      const { data }: { data: { session: Session | null } } = await supabase.auth.getSession();
 
-    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      if (!data.session) {
+        router.push('/auth/signin');
+      } else {
+        setSession(data.session);
+      }
+
+      setLoading(false);
+    };
+
+    getSession();
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
     });
 
-    return () => listener?.subscription.unsubscribe();
+    return () => subscription.unsubscribe();
   }, [router]);
 
   if (loading) return <p>Загрузка...</p>;
